e010a8c949bd237a225678c6eea839baa07afbac
GengHao <genghao@buaa.edu.cn>
2022-11-13 11:17:15 +0000

    update

--- v0/run_DBLP.sh
+++ v0/run_DBLP.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 【实验结果】
+# best_val_f1_micro 0.94     best_val_f1_micro_epoch 7
+# best_val_f1_macro 0.93948  best_val_f1_macro_epoch 13
+# best_test_f1_micro 0.94443 best_test_f1_micro_epoch 12
+# best_test_f1_macro 0.9394  best_test_f1_macro_epoch 12
+
 set -eux
 
 if [ ! -f main.py ]; then 
@@ -9,7 +15,7 @@ fi
 python main.py \
     --hg_path "/Dataset/PyG/DBLP/Processed/DBLP.dglhg.pkl" \
     --infer_ntype author \
-    --hidden_dim 256 \
+    --hidden_dim 400 \
     --num_layers 4 \
     --activation prelu \
     --num_epochs 200 \
